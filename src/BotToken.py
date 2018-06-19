167b734ef6cef5e9e05e88ccb43a534384b4ddf1
Kakumanu Asish <kkmnasish84@gmail.com>
2018-06-19 17:32:45 +0530

    Update BotToken.py

--- src/BotToken.py
+++ src/BotToken.py
@@ -3,7 +3,7 @@ from telepot.loop import MessageLoop
 
 
 def BotToken():
-    TOKEN = '568848013:AAENwGdsYyesAteYRtRoqHFYuj3cl-0PXZ0'  # get token from command-line
+    TOKEN = ''  # get token from command-line
 
     bot = telepot.Bot(TOKEN)
     return bot
